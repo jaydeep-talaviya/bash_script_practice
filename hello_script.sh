d4e7b2d1fa561f6e43613880f4b20a8bab77a814
jaydeep <jaydeeptalaviya8@gmail.com>
2024-06-25 21:57:24 +0530

    added comment

--- hello_script.sh
+++ hello_script.sh
@@ -2,4 +2,15 @@
 
 echo "hello bash script abc" > generated.txt
 
-cat > dynamic_file.txt
+# this is commend and not do anything. single line comment
+
+: 'line 1,
+line 2,
+line 3,
+line 4,
+line 5 , -> multi line comment
+'
+
+cat >> dynamic_file.txt
+
+echo "saved file"
