7d00cc845329b24beceb7550f21c1c3153ed009e
jaydeep <jaydeeptalaviya8@gmail.com>
2024-06-26 20:24:11 +0530

    added standard output

---
+++ standard_output.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+ls -al 1>input.txt 2>error.txt   
+# 1 for stadard output of command (which show in terminal by using ls -al)
+# 2 for any kind of error occor on command
+
+ls +al 1>input.txt 2>error.txt   
