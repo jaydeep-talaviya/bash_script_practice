548ca16b839bdf8ebeab9d310c49149ab0809d1c
jaydeep <jaydeeptalaviya8@gmail.com>
2024-06-26 06:50:31 +0530

    different syntax for multiple condition

--- conditional_statement.sh
+++ conditional_statement.sh
@@ -17,3 +17,14 @@ echo "this count is $count2"
 else
 echo "this count is not $count2"
 fi
+
+age=150
+
+if [[ $age -gt 18  &&  $age -lt 40 ]]
+then 
+    echo "i am adult"
+elif [[ $age -gt 12  &&  $age -lt 19 ]]
+then echo "i am teenager"
+else
+    echo "i am youger or older"
+fi
